53175a59571d4095f22702275c3888dc9c538cea
Energeenot <abramov.pandos@yandex.ru>
2025-02-15 20:15:13 +0300

    написал дто для запросов к данному сервису

---
+++ src/main/java/Energeenot/AuthService/dto/AuthRequest.java
@@ -0,0 +1,20 @@
+package Energeenot.AuthService.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthRequest {
+
+    @Email(message = "Неверный формат электронной почты")
+    @NotBlank(message = "Требуется ввести электронную почту")
+    private String email;
+
+    @NotBlank(message = "Требуется ввести пароль")
+    @Size(min = 8, message = "Пароль должен содержать не менее 8 символов")
+    private String password;
+}
