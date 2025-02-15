62fedab88a381e4a7f77a04599ee8612e2f7ab9a
Energeenot <abramov.pandos@yandex.ru>
2025-02-15 20:18:28 +0300

    написал Добавлен служебный класс JwtProvider для обработки генерации и проверки токенов JWT

---
+++ src/main/java/Energeenot/AuthService/util/JwtProvider.java
@@ -0,0 +1,38 @@
+package Energeenot.AuthService.util;
+
+import Energeenot.AuthService.models.enums.Role;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(String email, Role role) {
+        log.info("Generating JWT, email: {}, role: {}", email, role);
+        return JWT.create()
+                .withSubject(email)
+                .withClaim("role", role.name())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expiration))
+                .sign(Algorithm.HMAC256(secret));
+    }
+
+    public String validateToken(String token) {
+        log.info("Validating JWT, token: {}", token);
+        return JWT.require(Algorithm.HMAC256(secret))
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+}
