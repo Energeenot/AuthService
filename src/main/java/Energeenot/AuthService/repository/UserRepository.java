93ad422e87d1262dfca7a5d79d946a16cb01bf9f
Energeenot <abramov.pandos@yandex.ru>
2025-02-15 20:14:27 +0300

    репозиторий для работы с сущностью пользователя

---
+++ src/main/java/Energeenot/AuthService/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package Energeenot.AuthService.repository;
+
+import Energeenot.AuthService.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String mail);
+}
