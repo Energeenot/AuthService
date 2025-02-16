cd659ee3c89c574acc948bce9a9a88a4b6e70dff
Energeenot <abramov.pandos@yandex.ru>
2025-02-16 18:49:32 +0300

    добавил логику обновления refreshToken, так же теперь при логине передаётся jwt и refreshToken

--- src/main/java/Energeenot/AuthService/service/AuthService.java
+++ src/main/java/Energeenot/AuthService/service/AuthService.java
@@ -1,6 +1,7 @@
 package Energeenot.AuthService.service;
 
 import Energeenot.AuthService.dto.AuthRequest;
+import Energeenot.AuthService.dto.TokenResponse;
 import Energeenot.AuthService.exception.UserAlreadyExistsException;
 import Energeenot.AuthService.models.User;
 import Energeenot.AuthService.models.enums.Role;
@@ -20,7 +21,7 @@ public class AuthService {
     private final UserRepository userRepository;
     private final JwtProvider jwtProvider;
 
-    public String login(AuthRequest authRequest) {
+    public TokenResponse login(AuthRequest authRequest) {
         log.info("try to login with email: {}", authRequest.getEmail());
         User user = userRepository.findByEmail(authRequest.getEmail())
                 .orElseThrow(() -> new RuntimeException("User not found"));
@@ -29,13 +30,40 @@ public class AuthService {
             throw new RuntimeException("Wrong password");
         }
 
-        return jwtProvider.generateToken(authRequest.getEmail(), user.getRole());
+        String accessToken = jwtProvider.generateToken(authRequest.getEmail(), user.getRole());
+        String refreshToken = jwtProvider.generateRefreshToken(authRequest.getEmail());
+
+        user.setRefreshToken(passwordEncoder.encode(refreshToken));
+        userRepository.save(user);
+
+        return new TokenResponse(accessToken, refreshToken);
+    }
+
+    public TokenResponse refresh(String refreshToken) {
+        String email = jwtProvider.validateToken(refreshToken);
+        log.info("refreshing token for user with email: {}", email);
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(refreshToken, user.getRefreshToken())){
+            throw new RuntimeException("Invalid refresh token");
+        }
+
+        String accessToken = jwtProvider.generateToken(email, user.getRole());
+        String newRefreshToken = jwtProvider.generateRefreshToken(email);
+
+        user.setRefreshToken(passwordEncoder.encode(refreshToken));
+        userRepository.save(user);
+
+        return new TokenResponse(accessToken, newRefreshToken);
     }
 
     public String registration(AuthRequest authRequest) {
         log.info("Registering user with email: {}", authRequest.getEmail());
         if (userRepository.findByEmail(authRequest.getEmail()).isPresent()) {
-            throw new UserAlreadyExistsException(String.format("User with email: '%s' already exists", authRequest.getEmail()));
+            throw new UserAlreadyExistsException(String.format("User with email: '%s' already exists",
+                    authRequest.getEmail()));
         }
         User user = new User();
         user.setEmail(authRequest.getEmail());
