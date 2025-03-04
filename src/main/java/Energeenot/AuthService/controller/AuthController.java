d2045fda47bf537904f2745ba2c52281aed45156
Energeenot <abramov.pandos@yandex.ru>
2025-03-04 15:36:32 +0300

    метод регистрации теперь возвращает дто, в каждом методе указал produces = MediaType.APPLICATION_JSON_VALUE

--- src/main/java/Energeenot/AuthService/controller/AuthController.java
+++ src/main/java/Energeenot/AuthService/controller/AuthController.java
@@ -1,11 +1,13 @@
 package Energeenot.AuthService.controller;
 
 import Energeenot.AuthService.dto.AuthRequest;
+import Energeenot.AuthService.dto.RegistrationResponse;
 import Energeenot.AuthService.dto.TokenResponse;
 import Energeenot.AuthService.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,22 +22,21 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/login")
+    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<TokenResponse> login(@Valid @RequestBody AuthRequest authRequest) {
         log.info("Login request with email: {}", authRequest.getEmail());
         return ResponseEntity.ok(authService.login(authRequest));
     }
 
-    @PostMapping("/refresh")
+    @PostMapping(value = "/refresh", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<TokenResponse> refresh(@RequestBody String refreshToken) {
         log.info("Refresh request with refresh token: {}", refreshToken);
         return ResponseEntity.ok(authService.refresh(refreshToken));
     }
 
-    @PostMapping("/registration")
-    public ResponseEntity<String> registration(@Valid @RequestBody AuthRequest authRequest) {
+    @PostMapping(value = "/registration", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<RegistrationResponse> registration(@Valid @RequestBody AuthRequest authRequest) {
         log.info("Registration request with email: {}", authRequest.getEmail());
-        String message = authService.registration(authRequest);
-        return ResponseEntity.ok(message);
+        return ResponseEntity.ok(authService.registration(authRequest));
     }
 }
