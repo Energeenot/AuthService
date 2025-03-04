f891535459e0d599b720be403daca60a0e33530f
Energeenot <abramov.pandos@yandex.ru>
2025-03-04 15:44:13 +0300

    добавлен бин securityFilterChain для настройки правил доступа

--- src/main/java/Energeenot/AuthService/configuration/SecurityConfig.java
+++ src/main/java/Energeenot/AuthService/configuration/SecurityConfig.java
@@ -4,10 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/registration", "/auth/login", "/auth/refresh").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
