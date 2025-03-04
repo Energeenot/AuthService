c3d057958ee177f8021e8a198759801c3ac6defc
Energeenot <abramov.pandos@yandex.ru>
2025-03-04 15:50:15 +0300

    добавил аннотацию Getter для преобразования объекта в JSON

--- src/main/java/Energeenot/AuthService/dto/TokenResponse.java
+++ src/main/java/Energeenot/AuthService/dto/TokenResponse.java
@@ -2,9 +2,13 @@ package Energeenot.AuthService.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Builder
+@ToString
+@Getter
 public class TokenResponse {
     private String accessToken;
     private String refreshToken;
